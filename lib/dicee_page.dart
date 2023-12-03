ff48d2cf18a790c23a5c8a11db8e4a72ca6e009c
zhuikovda <dmitiy.zhuikov@janix-soft.com>
2023-12-03 15:14:07 +0300

    feat: L53

--- lib/dicee_page.dart
+++ lib/dicee_page.dart
@@ -8,14 +8,24 @@ class DicePage extends StatelessWidget {
         children: [
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  print('left');
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
             ),
           ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice2.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  print('right');
+                },
+                child: Image.asset('images/dice2.png'),
+              ),
             ),
           ),
         ],
