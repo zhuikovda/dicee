3050d4acebeba95e79204327b6aa3b8d97382321
zhuikovda <dmitiy.zhuikov@janix-soft.com>
2023-12-03 14:37:57 +0300

    feat: L52

--- lib/dicee_page.dart
+++ lib/dicee_page.dart
@@ -3,15 +3,23 @@ import 'package:flutter/material.dart';
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice2.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
